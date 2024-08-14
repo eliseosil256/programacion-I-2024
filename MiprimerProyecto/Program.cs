6d91597c30132cd34ea8d1f0ce1edea3841d7d27
eliseosil256 <eliseosil2005@gmail.com>
2024-08-14 10:09:07 -0600

    determinar si cada numero de la serie es par o impar

--- MiprimerProyecto/Program.cs
+++ MiprimerProyecto/Program.cs
@@ -10,7 +10,7 @@ namespace MiprimerProyecto
     {
         static void Main(string[] args)
         {
-            //determinar si cada numero de la serie es par o impar
+            //determinar si cada numero de la serie es par o impar :D
             int[] serie = new int[] { 5, 9, 4, 6, 3, 2, };
             foreach(int num in serie) {
                 Console.WriteLine("el num {0} es {1}", num, num % 2 == 0 ? "Par" : "impar");
