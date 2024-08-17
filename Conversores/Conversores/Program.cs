2667e99aa42928e795c71d7d1b6ad3ff46060aa8
eliseosil256 <eliseosil2005@gmail.com>
2024-08-17 17:15:40 -0600

    Conversor de Longitud

--- Conversores/Conversores/Program.cs
+++ Conversores/Conversores/Program.cs
@@ -10,39 +10,37 @@ namespace Conversores
     {
         static void Main(string[] args)
         {
-            string[] unidades = { "Litros", "Mililitros", "Galones estadounidenses", "Pintas estadounidenses", "Tazas", "Onzas líquidas estadounidenses", "Metros cúbicos", "Centímetros cúbicos", "Barriles de petróleo", "Pies cúbicos" };
+            string[] unidades = { "Metros", "Kilómetros", "Centímetros", "Milímetros", "Micrómetros", "Nanómetros", "Millas", "Yardas", "Pies", "Pulgadas" };
 
             double[,] factoresConversion = {
-            { 1,          1000,       0.264172,       2.11338,       4.22675,       33.814,       0.001,       1000,       0.00628981,       0.0353147 }, // Litro
-            { 0.001,      1,          0.000264172,    0.00211338,    0.00422675,    0.033814,     1e-6,        1,          6.2898e-6,        3.5315e-5 }, // Mililitro
-            { 3.78541,    3785.41,    1,              8,             16,            128,          0.00378541,  3785.41,    0.0238095,        0.133681 },  // Galon
-            { 0.473176,   473.176,    0.125,          1,             2,             16,           0.000473176, 473.176,    0.00297619,       0.0167101 }, // Pinta estadounidense
-            { 0.236588,   236.588,    0.0625,         0.5,           1,             8,            0.000236588, 236.588,    0.0014881,        0.00835503 },// Taza
-            { 0.0295735,  29.5735,    0.0078125,      0.0625,        0.125,         1,            2.9574e-5,   29.5735,    9.3206e-5,        0.00104438 },// Onza líquida estadounidense
-            { 1000,       1e6,        264.172,        2113.38,       4226.75,       33814,        1,           1e6,        6.28981,          35.3147 },   // Metro cúbico
-            { 0.001,      1,          2.64172e-4,     0.00211338,    0.00422675,    0.033814,     1e-6,        1,          6.2898e-6,        3.5315e-5 }, // Centímetro cúbico
-            { 159,        159000,     42,             336,           672,           5376,         0.159,       159000,     1,                5.61458 },   // Barril de petróleo
-            { 28.3168,    28316.8,    7.48052,        59.8442,       119.688,       957.506,      0.0283168,   28316.8,    0.178108,         1 }          // Pie cúbico
+            //Metro      //Kilometro    //Centimetro //Milimetro   //MicroM    //NanoM      //Milla              //Yarda          //Pie             //Pulgada
+            { 1,         0.001,         100,         1000,         1e6,        1e9,         0.000621371,         1.09361,         3.28084,          39.3701 },   // Metro
+            { 1000,      1,             100000,      1e6,          1e9,        1e12,        0.621371,            1093.61,         3280.84,          39370.1 },   // Kilómetro
+            { 0.01,      1e-5,          1,           10,           10000,      1e7,         6.2137e-6,           0.0109361,       0.0328084,        0.393701 },  // Centímetro
+            { 0.001,     1e-6,          0.1,         1,            1000,       1e6,         6.2137e-7,           0.00109361,      0.00328084,       0.0393701 }, // Milímetro
+            { 1e-6,      1e-9,          1e-4,        0.001,        1,          1000,        6.2137e-10,          1.0936e-6,       3.2808e-6,        3.937e-5 },  // Micrómetro
+            { 1e-9,      1e-12,         1e-7,        1e-6,         0.001,      1,           6.2137e-13,          1.0936e-9,       3.2808e-9,        3.937e-8 },  // Nanómetro
+            { 1609.34,   1.60934,       160934,      1.609e6,      1.609e9,    1.609e12,    1,                   1760,            5280,             63360 },     // Milla
+            { 0.9144,    0.0009144,     91.44,       914.4,        914400,     9.144e8,     0.000568182,         1,               3,                36 },        // Yarda
+            { 0.3048,    0.0003048,     30.48,       304.8,        304800,     3.048e8,     0.000189394,         0.333333,        1,                12 },        // Pie
+            { 0.0254,    2.54e-5,       2.54,        25.4,         25400,      2.54e7,      1.5783e-5,           0.0277778,       0.0833333,        1 }          // Pulgada
         };
-            Console.WriteLine("Seleccione la unidad de volumen de entrada:");
+            Console.WriteLine("Seleccione la unidad de longitud de entrada:");
             for (int i = 0; i < unidades.Length; i++)
             {
                 Console.WriteLine($"{i + 1}. {unidades[i]}");
             }
             int unidadEntrada = int.Parse(Console.ReadLine()) - 1;
 
-            Console.WriteLine("Seleccione la unidad de volumen de salida:");
+            Console.WriteLine("Seleccione la unidad de longitud de salida:");
             for (int i = 0; i < unidades.Length; i++)
             {
                 Console.WriteLine($"{i + 1}. {unidades[i]}");
             }
             int unidadSalida = int.Parse(Console.ReadLine()) - 1;
-
             Console.WriteLine("Ingrese la cantidad a convertir:");
             double cantidad = double.Parse(Console.ReadLine());
-
             double resultado = cantidad * factoresConversion[unidadEntrada, unidadSalida];
-
             Console.WriteLine($"{cantidad} {unidades[unidadEntrada]} es igual a {resultado} {unidades[unidadSalida]}.");
             Console.ReadLine();
         }
