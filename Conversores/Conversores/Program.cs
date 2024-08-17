3f46fb3fa40bee10e8eb6ebc4f783d6775d4c020
eliseosil256 <eliseosil2005@gmail.com>
2024-08-17 16:59:31 -0600

    Conversor de volumen

--- Conversores/Conversores/Program.cs
+++ Conversores/Conversores/Program.cs
@@ -10,30 +10,28 @@ namespace Conversores
     {
         static void Main(string[] args)
         {
-            string[] unidades = { "Gramos", "Kilogramos", "Miligramos", "Toneladas métricas", "Libras", "Onzas", "Stones", "Granos", "Microgramos", "Quintales métricos" };
+            string[] unidades = { "Litros", "Mililitros", "Galones estadounidenses", "Pintas estadounidenses", "Tazas", "Onzas líquidas estadounidenses", "Metros cúbicos", "Centímetros cúbicos", "Barriles de petróleo", "Pies cúbicos" };
 
             double[,] factoresConversion = {
-            //Gramo         //Kilogramo //MiliGr //Tonelada    //Libra           //Onza              //Stone        //Grano      //MicroGr //Quintal
-            { 1,            0.001,      1000,    1e-6,         0.00220462,       0.03527396,         0.00015747,    15.4324,     1e6,      1e-5        }, // Gramo
-            { 1000,         1,          1e6,     0.001,        2.20462,          35.27396,           0.15747,       15432.4,     1e9,      0.01        }, // Kilogramo
-            { 0.001,        1e-6,       1,       1e-9,         2.2046e-6,        3.5274e-5,          1.5747e-7,     0.015432,    1000,     1e-8        }, // Miligramo
-            { 1e6,          1000,       1e9,     1,            2204.62,          35273.96,           157.47,        15432400,    1e12,     10          }, // Tonelada
-            { 453.592,      0.453592,   453592,  0.000453592,  1,                16,                 0.0714286,     7000,        4.536e8,  0.00453592  }, // Libra
-            { 28.3495,      0.0283495,  28349.5, 2.835e-5,     0.0625,           1,                  0.00446429,    437.5,       2.835e7,  0.000283495 }, // Onza
-            { 6350.29,      6.35029,    6.35e6,  0.00635029,   14,               224,                1,             98000,       6.35e9,   0.0635029   }, // Stone
-            { 0.0647989,    6.47989e-5, 64.7989, 6.4799e-8,    0.000142857,      0.00228571,         1.02041e-5,    1,           64798.9,  6.47989e-7  }, // Grano
-            { 1e-6,         1e-9,       0.001,   1e-12,        2.2046e-9,        3.5274e-8,          1.5747e-10,    1.5432e-5,   1,        1e-11       }, // Microgramo
-            { 100000,       100,        1e8,     0.1,          220.462,          3527.4,             15.747,        1543240,     1e11,     1           }  // Quintal
+            { 1,          1000,       0.264172,       2.11338,       4.22675,       33.814,       0.001,       1000,       0.00628981,       0.0353147 }, // Litro
+            { 0.001,      1,          0.000264172,    0.00211338,    0.00422675,    0.033814,     1e-6,        1,          6.2898e-6,        3.5315e-5 }, // Mililitro
+            { 3.78541,    3785.41,    1,              8,             16,            128,          0.00378541,  3785.41,    0.0238095,        0.133681 },  // Galon
+            { 0.473176,   473.176,    0.125,          1,             2,             16,           0.000473176, 473.176,    0.00297619,       0.0167101 }, // Pinta estadounidense
+            { 0.236588,   236.588,    0.0625,         0.5,           1,             8,            0.000236588, 236.588,    0.0014881,        0.00835503 },// Taza
+            { 0.0295735,  29.5735,    0.0078125,      0.0625,        0.125,         1,            2.9574e-5,   29.5735,    9.3206e-5,        0.00104438 },// Onza líquida estadounidense
+            { 1000,       1e6,        264.172,        2113.38,       4226.75,       33814,        1,           1e6,        6.28981,          35.3147 },   // Metro cúbico
+            { 0.001,      1,          2.64172e-4,     0.00211338,    0.00422675,    0.033814,     1e-6,        1,          6.2898e-6,        3.5315e-5 }, // Centímetro cúbico
+            { 159,        159000,     42,             336,           672,           5376,         0.159,       159000,     1,                5.61458 },   // Barril de petróleo
+            { 28.3168,    28316.8,    7.48052,        59.8442,       119.688,       957.506,      0.0283168,   28316.8,    0.178108,         1 }          // Pie cúbico
         };
-
-            Console.WriteLine("Seleccione la unidad de masa de entrada:");
+            Console.WriteLine("Seleccione la unidad de volumen de entrada:");
             for (int i = 0; i < unidades.Length; i++)
             {
                 Console.WriteLine($"{i + 1}. {unidades[i]}");
             }
             int unidadEntrada = int.Parse(Console.ReadLine()) - 1;
 
-            Console.WriteLine("Seleccione la unidad de masa de salida:");
+            Console.WriteLine("Seleccione la unidad de volumen de salida:");
             for (int i = 0; i < unidades.Length; i++)
             {
                 Console.WriteLine($"{i + 1}. {unidades[i]}");
